2cd454cf57a20f07deee478dadd2dc7cffc5e6f6
Leonardo Di Matteo <leonardodimatteo94@gmail.com>
2023-11-22 17:03:47 +0100

    Start run

--- run.rs
+++ run.rs
@@ -1,15 +1,27 @@
 // Commandline to Compile either Rust or C++ in P..
-use std::{io, env};
+use std::{env, io};
+
+fn compile_rust() -> io::Result<()> {
+    // TODO! Compile Rust
+    Ok(())
+}
+
+fn compile_cpp() -> io::Result<()> {
+    // TODO! Compile C++
+    Ok(())
+}
 
 fn main() -> io::Result<()> {
-    let run_type = env::args().nth(1).unwrap();
+    let run_type = env::args().nth(1)
 
-    match run_type {
-        "r" | _: {
-
-    }, 
-        "c" | 
-    }
+    match run_type.as_str() {
+        "-c" => {
+            compile_cpp()?;
+                }
+        "-r" | _ => {
+            compile_rust()?;
+        }
+    };
 
     Ok(())
 }
