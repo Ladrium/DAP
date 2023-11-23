f2fbea0572fb1f667391b32e8c3f8c04c000b7dc
Leonardo Di Matteo <leonardodimatteo94@gmail.com>
2023-11-23 20:23:18 +0100

    Run CLI

--- P5.1/P5-1.rs
+++ P5.1/P5-1.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("DDD");
-} 
+    let mut line = String::new();
+    println!("Enter your name :");
+    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Test , {}", line);
+}
--- run.rs
+++ run.rs
@@ -1,25 +1,75 @@
 // Commandline to Compile either Rust or C++ in P..
-use std::{env, io};
+use std::{env, io, process};
 
-fn compile_rust() -> io::Result<()> {
-    // TODO! Compile Rust
-    Ok(())
+fn compile(file: String, file_type: &str) -> io::Result<()> {
+    let dir = env::current_dir()?;
+
+    let command = match file_type {
+        "rs" => "rustc",
+        "cpp" => "g++",
+        _ => "rustc",
+    };
+
+    let formatted_file = file.replace('.', "-");
+    let input = format!(
+        "{}/{}/{}.{}",
+        dir.display(),
+        file,
+        formatted_file,
+        file_type
+    );
+    let output = format!("{}/{}/{}.exe", dir.display(), file, formatted_file);
+
+    let cmd = process::Command::new(command)
+        .arg(input)
+        .arg("-o")
+        .arg(&output)
+        .output()?;
+
+    if cmd.status.success() {
+        println!("Compiled!");
+        execute(output);
+        Ok(())
+    } else {
+        eprintln!("Compilation failed: {:?}", cmd);
+        Err(io::Error::new(io::ErrorKind::Other, "Compilation failed"))
+    }
 }
 
-fn compile_cpp() -> io::Result<()> {
-    // TODO! Compile C++
-    Ok(())
+fn execute(file_path: String) {
+    process::Command::new(file_path)
+        .status()
+        .expect("Failed to execute");
+
+    println!("Finished Program!");
 }
 
 fn main() -> io::Result<()> {
-    let run_type = env::args().nth(1)
+    let mut args = env::args();
+    args.next();
+    let len = args.len();
+    let run_type: String;
 
-    match run_type.as_str() {
+    if len == 0 {
+        println!("Aufgabe nicht gegeben: <run> -r|-c <Aufgabe(P5.1)>");
+        return Ok(());
+    } else if len == 1 {
+        run_type = String::from("-c");
+    } else {
+        run_type = args.next().unwrap();
+    }
+
+    let file = args.next().unwrap();
+
+    match run_type.to_lowercase().as_str() {
         "-c" => {
-            compile_cpp()?;
-                }
-        "-r" | _ => {
-            compile_rust()?;
+            compile(file, "cpp")?;
+        }
+        "-r" => {
+            compile(file, "rs")?;
+        }
+        _ => {
+            println!("Aufgabe nicht gegeben: <run> -r|-c <Aufgabe(P5.1)>");
         }
     };
 
